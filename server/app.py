6ac734e23badc4ef823b1989ea626dc83490484c
Emmett Underhill <emmett.underhill@instacart.com>
2023-12-06 16:17:48 -0500

    Add basic echo websocket server

--- server/app.py
+++ server/app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+from flask import Flask
+from flask_sock import Sock
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app.config['SECRET_KEY'] = 'your secret key'
+sock = Sock(app)
+
+@sock.route('/')
+def echo(ws):
+    print(ws.environ)
+    while True:
+        data = ws.receive()
+        ws.send(data)
