ff104e59fb1af7dae06a49dc06e3d5429e1028f9
Emmett Underhill <emmett.underhill@instacart.com>
2024-01-02 17:50:57 -0500

    Switching to tf-idf to redact information

--- server/wiki.py
+++ server/wiki.py
@@ -10,9 +10,17 @@ CHARACTER_MIN = 120
 
 REDACT_CHARACTER_MIN = 4
 
-REDACT_THRESHOLD = 0.75
+# REDACT_THRESHOLD = 0.75
+REDACT_THRESHOLD = 6
 
 class WikiScraper():
+
+    def __init__(self):
+        with open("./config/word_ranks.txt", "r") as word_rank_txt:
+            words = word_rank_txt.read().splitlines()
+            self.word_ranks = {}
+            for index, word in enumerate(words):
+                self.word_ranks[word] = index
 
     def init_page(self, page):
         html = requests.get(f"{WIKI_URL}/{page}").text
@@ -23,33 +31,51 @@ class WikiScraper():
         paragraphs = [p for p in paragraphs if len(p) > CHARACTER_MIN]
 
         paragraphs = [re.sub("\[.*?\]", "", p) for p in paragraphs]
-
-        paragraphs = [self.redact_paragraph(p, page) for p in paragraphs]
-
         self.paragraphs = paragraphs
-        
+
+        self.redact_idf()
+
 
     def get_random_paragraph(self):
         return random.choice(self.paragraphs)
 
-    def redact_paragraph(self, paragraph, page_name):
-        banned_words = set()
-        name_word_length = len(page_name.split())
-        name_length = len(page_name)
-        words = paragraph.split()
-        for index, _ in enumerate(words[0:-name_word_length]):
-            window = words[index:index + name_word_length]
-            phrase = " ".join(window)
-            if editdistance.eval(phrase.lower(), page_name.lower()) / name_length < REDACT_THRESHOLD:
-                banned_words.add(phrase)
+    def redact_idf(self):
+        page_word_counts = {}
+        for paragraph in self.paragraphs:
+            for word in paragraph.split():
+                token = self.tokenize(word)
+                page_word_counts[token] = 1 if token not in page_word_counts else page_word_counts[token] + 1
+
+        page_word_ranks = sorted(list(page_word_counts.keys()), key=lambda w: page_word_counts[w], reverse=True)
+        self.page_word_ranks = {}
+
+        for index, word in enumerate(page_word_ranks):
+            self.page_word_ranks[word] = index
         
-        length_sorted_words = sorted(list(banned_words), key=len, reverse=True)
-        for word in length_sorted_words:
-            paragraph = paragraph.replace(word, "[redacted]")
-        return paragraph
+        for index, paragraph in enumerate(self.paragraphs):
+            banned_words = set()
+            clean_words = []
+            for word in paragraph.split():
+                token = self.tokenize(word)
+                
+                if len(token) <= 1:
+                    continue
+
+                trp = (self.word_ranks[token] + 1) / len(self.word_ranks) if token in self.word_ranks else 1
+                drp = (self.page_word_ranks[token] + 1) / len(self.page_word_ranks) if token in self.page_word_ranks else 1
+
+                importance = trp * (1 / drp)
+                clean_words.append(word if importance < REDACT_THRESHOLD else "[redacted]")
+
+            self.paragraphs[index] = " ".join(clean_words) + "\n"
+
+
+    def tokenize(self, word):
+        fn = lambda x: x.isalpha() or x == "."
+        return "".join(filter(fn, word)).lower()
 
 if __name__ == "__main__":
     scraper = WikiScraper()
-    scraper.init_page("Mali")
-
+    scraper.init_page("Germany")
     print(scraper.get_random_paragraph())
+
